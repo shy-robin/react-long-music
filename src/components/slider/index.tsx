4c2282143d329f92c5588e78af012c8dd531452d
ShyRobin <shy-robin@163.com>
2022-07-21 10:23:11 +0800

    feat: 推荐页轮播图

---
+++ src/components/slider/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+import commonStyle from '../../assets/css/commonStyle'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Autoplay } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+
+const themeColor = commonStyle['theme-color']
+
+const SliderContainer = styled.div`
+  img {
+    width: 100%;
+  }
+  .swiper-pagination-bullet-active {
+    background-color: ${themeColor} !important;
+  }
+`
+
+interface Banner {
+  imgUrl: string
+}
+
+interface SliderProps {
+  bannerList: Banner[]
+}
+
+const Slider = (props: SliderProps) => {
+  const { bannerList } = props
+
+  return (
+    <SliderContainer>
+      <Swiper
+        loop
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[Pagination, Autoplay]}
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        {bannerList.map((banner, index) => (
+          <SwiperSlide key={index}>
+            <img src={banner.imgUrl} alt="banner" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </SliderContainer>
+  )
+}
+
+export default React.memo(Slider)
--- src/pages/recommend/index.tsx
+++ src/pages/recommend/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Slider from '../../components/slider'
 
 const Recommend = () => {
+  // mock
+  const bannerList = [1,2,3].map(item => {
+    return {
+      imgUrl:
+        'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg',
+    }
+  })
   return (
-    <div>Recommend</div>
+    <div>
+      <Slider bannerList={bannerList}></Slider>
+    </div>
   )
 }
 
